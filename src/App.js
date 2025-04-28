9dfa33ecc8b200482a7e490f7144205b564dfdc2
jonahwhitney <jonahwhitney@yahoo.com>
2025-04-28 12:49:21 -0500

    Practicing with react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import restaurant from './restaurant.jpg';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  )
+}
+
+function Main(props) {
+  return (
+    <section>
+      <p>We serve {props.adj} food.</p>
+      <img src={restaurant} height={320} alt="Busy outdoor service at a restaurant" />
+      <ul style={{ textAlign: "left" }}>
+        {props.dishes.map((dish) => (
+          <li key={dish.id}>{dish.title}</li>
+        ))}
+      </ul>
+    </section>
+  )
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <h3>No food-ier food has ever been cooked. It is {props.year}.</h3>
+    </footer>
+  )
+}
+
+const dishes = [
+  "Bistec a la Mexicano",
+  "Pepper crusted ribeye steak",
+  "Fried ice cream",
+  "All of the fajitas!"
+];
+const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
+console.log(dishObjects);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Jonah" />
+      <Main adj="yummy" dishes={dishObjects} />
+      <Footer year={new Date().getFullYear()} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ul>
-    <li>January</li>
-    <li>February</li>
-    <li>March</li>
-    <li>April</li>
-    <li>May</li>
-    <li>June</li>
-    <li>July</li>
-    <li>August</li>
-    <li>September</li>
-    <li>October</li>
-    <li>November</li>
-    <li>December</li>
-  </ul>
+  <App />
 );
 
