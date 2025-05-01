8bf6e6907c547c1f4541c96f0c9550f4981156c0
jonahwhitney <jonahwhitney@yahoo.com>
2025-05-01 11:17:39 -0500

    Continuing react essentials - useState, destructuring, conditional rendering.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
+import React, { useState } from 'react';
 import './App.css';
 import restaurant from './restaurant.jpg';
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  )
+function TrueFalseLabel() {
+  return <h1>True or false?</h1>
 }
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve {props.adj} food.</p>
-      <img src={restaurant} height={320} alt="Busy outdoor service at a restaurant" />
-      <ul style={{ textAlign: "left" }}>
-        {props.dishes.map((dish) => (
-          <li key={dish.id}>{dish.title}</li>
-        ))}
-      </ul>
-    </section>
-  )
+function BengalsRBLabel() {
+  return <h1>The Bengal's Running Back is...</h1>
 }
 
-function Footer(props) {
-  return (
-    <footer>
-      <h3>No food-ier food has ever been cooked. It is {props.year}.</h3>
-    </footer>
-  )
+function TrueComponent() {
+  return <h3>The Cincinnati Bengals are a football team.</h3>
 }
 
-const dishes = [
-  "Bistec a la Mexicano",
-  "Pepper crusted ribeye steak",
-  "Fried ice cream",
-  "All of the fajitas!"
-];
-const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
-console.log(dishObjects);
+function FalseComponent() {
+  return <h3>The Cincinnati Bengals are a well run franchise.</h3>
+}
 
-function App() {
+function App({ authorized }) {
+  const [qb, wr1, wr2, rb, lt] = ["Joe Burrow", "Ja'Marr Chase", "Tee Higgins", "Chase Brown", "Orlando Brown Jr"];
+  const [carState, setCarState] = useState("cruising");
+
   return (
-    <div className="App">
-      <Header name="Jonah" />
-      <Main adj="yummy" dishes={dishObjects} />
-      <Footer year={new Date().getFullYear()} />
-    </div>
-  );
+    <>
+      <TrueFalseLabel />
+      {authorized ? <TrueComponent /> : <FalseComponent />}
+      <BengalsRBLabel />
+      <h3>{qb}? {wr1}? {wr2}? {rb}? {lt}?</h3>
+      <h2>The Bengals running back is {rb}!!!</h2>
+
+      <h1>State of the car is {carState}.</h1>
+      <button onClick={() => setCarState("accelerating")}>Accelerate</button>
+      <button onClick={() => setCarState("braking")}>Brake</button>
+      <button onClick={() => setCarState("cruising")}>Cruise</button>
+    </>
+  )
 }
 
 export default App;
